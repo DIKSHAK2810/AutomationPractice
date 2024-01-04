267ccc69fe93889e076c1abc3545ce2ef54b00a7
DIKSHAK2810 <124604161+DIKSHAK2810@users.noreply.github.com>
2024-01-04 17:53:35 +0530

    added3

---
+++ PracticeBatch3/src/interface_Examples/Class1.java
@@ -0,0 +1,7 @@
+package interface_Examples;
+
+public interface Class1 {
+
+	int a =20;
+	public abstract void ab();
+}
---
+++ PracticeBatch3/src/interface_Examples/ClassA.java
@@ -0,0 +1,16 @@
+package interface_Examples;
+
+public  class ClassA implements Class1{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void ab() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
